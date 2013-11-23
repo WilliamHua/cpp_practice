11182d23ffee6c1b66b7711248c6b16b51ec9670
WilliamHua <wiliambinghua@gmail.com>
2013-11-23 02:46:14 -0600

    .

---
+++ tsp/tsp.cpp
@@ -0,0 +1,23 @@
+/* 
+ * http://www.psychicorigami.com/2007/04/17/tackling-the-travelling-salesman-problem-part-one/ 
+ * The traveling salesman problem is
+ *
+ */ 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> traversal;
+//Define a traversal as a set of cities (encoded with ints)
+//Define a distance matrix where, given the two cities, returns the distance
+int testMethod(){
+    return 0;
+}
+
+int main(){
+    cout << "helloworld" << endl;
+    testMethod();
+    return 0;
+}
+
