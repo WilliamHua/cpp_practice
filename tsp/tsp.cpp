10246eb763b390f26eedd6b06fe041ef7bedab30
William Hua <whua@cs.utexas.edu>
2013-11-24 02:21:39 -0600

    Finished the tsp with stocashtic hill climbing

--- tsp/tsp.cpp
+++ tsp/tsp.cpp
@@ -6,11 +6,13 @@
 
 #include <iostream>
 #include <stdlib.h>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
 vector<int> traversal;
 vector< vector<int> > distances; //distance matrix between two cities
+int cityNums = 100;
 
 vector< vector<int> > genDistances(int numCities){
     vector< vector<int> > cities(numCities, vector<int>(numCities));
@@ -27,36 +29,53 @@ vector< vector<int> > genDistances(int numCities){
 int genDistance(vector<int> traversal, vector< vector<int> > distances){
     int sum = 0;
     for(int i = 1; i < traversal.size(); i++){
-       sum += distances[i-1][i];
+       sum += distances[traversal[i-1]][traversal[i]];
     }
     return sum;
 }
 
-void swap(vector<int>& data, int a, int b){
-    int x = data.at(a);
-    data.at(a) = data.at(b);
-    data.at(b) = x;
+vector<int> swap(vector<int> data, int a, int b){
+    vector<int> new_data(data.size());
+    new_data = data;
+    int x = new_data.at(a);
+    new_data.at(a) = new_data.at(b);
+    new_data.at(b) = x;
+    return new_data;
 }
 
 vector<int> randSolution(){
+    int solution[100] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};//cityNums
+    vector<int> randSolution(&solution[0], &solution[0] + 100);
+    random_shuffle(randSolution.begin(), randSolution.end());
+    return randSolution;
 }
 
-vector<int> climbHill(){
+vector<int> climbHill(int numIterations, vector<int> traversal, vector< vector<int> > distances){
+    int counter = 0;
+    vector<int> new_traversal;
+    while(counter < numIterations){
+        int y = rand() % cityNums;
+        int x = rand() % cityNums;
+        while(x == y) y = rand() % cityNums;
+        new_traversal = swap(traversal, x, y);
+        if(genDistance(new_traversal, distances) < genDistance(traversal, distances)){
+            counter = 0;
+            traversal = new_traversal;
+        }else{
+            counter++;
+        }
+    }
+    return new_traversal;
 }
 
 int main(){
     srand(time(NULL));
-    distances = genDistances(10);
-    for(int x = 0; x < distances.size(); x++){
-        for(int y = 0; y < distances[0].size(); y++){
-            cout << distances[x][y] << " "; 
-        }
-        cout << endl;
-    }
-
-    int solution[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    vector<int> randSolution(begin(solution), end(solution));
-    //print out all of this 
+    distances = genDistances(cityNums);
+    traversal = randSolution(); 
+    cout << "The original solution had a distance of: " << genDistance(traversal, distances) << endl;
+    traversal = climbHill(100000, traversal, distances);
+    cout << "The new solution has a distance of: " << genDistance(traversal, distances) << endl;
+    
     return 0;
 }
 
