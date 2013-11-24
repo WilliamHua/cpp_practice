0967f19e19f75fbf230053f5244d3923ef55f414
William Hua <whua@cs.utexas.edu>
2013-11-23 21:55:49 -0600

    Almost finished

--- tsp/tsp.cpp
+++ tsp/tsp.cpp
@@ -1,7 +1,7 @@
 /* 
- * http://www.psychicorigami.com/2007/04/17/tackling-the-travelling-salesman-problem-part-one/ 
- * The traveling salesman problem is
- *
+ * http://www.psychicorigami.com/2007/04/17/tackling-the-travelling-salesman-
+ * problem-part-one/ 
+ * TLDR: Stochastic hill climbing to solve traveling salesman problem
  */ 
 
 #include <iostream>
@@ -10,32 +10,52 @@
 using namespace std;
 
 vector<int> traversal;
-vector< vector<int> > distances;
+vector< vector<int> > distances; //distance matrix between two cities
 
-vector< vector<int> > getDistances(int x, int y){
-    vector< vector<int> > cities(x, vector<int>(y));
-    for(int i = 0; i < x*y; i++){
-        for(int j = 0; j < x*y; j++){ 
+vector< vector<int> > genDistances(int numCities){
+    vector< vector<int> > cities(numCities, vector<int>(numCities));
+    for(int i = 0; i < numCities; i++){
+        for(int j = 0; j < i; j++){ 
             cities[i][j] = rand() % 1000 + 1;
+            //dist between a and b in this case is same as dist between b and a
+            cities[j][i] = cities[i][j]; 
         }
     }
     return cities;
 }
 
+int genDistance(vector<int> traversal, vector< vector<int> > distances){
+    int sum = 0;
+    for(int i = 1; i < traversal.size(); i++){
+       sum += distances[i-1][i];
+    }
+    return sum;
+}
 
-//Define a traversal as a set of cities (encoded with ints)
-//Define a distance matrix where, given the two cities, returns the distance
 void swap(vector<int>& data, int a, int b){
     int x = data.at(a);
     data.at(a) = data.at(b);
     data.at(b) = x;
 }
 
+vector<int> randSolution(){
+}
+
+vector<int> climbHill(){
+}
+
 int main(){
-    distances = getDistances(10, 10);
+    srand(time(NULL));
+    distances = genDistances(10);
     for(int x = 0; x < distances.size(); x++){
-        cout << distances[0][x] << endl; 
+        for(int y = 0; y < distances[0].size(); y++){
+            cout << distances[x][y] << " "; 
+        }
+        cout << endl;
     }
+
+    int solution[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<int> randSolution(begin(solution), end(solution));
     //print out all of this 
     return 0;
 }
